d7a87d94f7ea05a867c0ba8189935fcbbcb97ec9
Larshle <125282510+Larshle@users.noreply.github.com>
2023-03-26 17:25:04 +0200

    implementert uart i samsvar med knapper og lys

--- 2_uart/main.c
+++ 2_uart/main.c
@@ -1,10 +1,45 @@
 #include <stdint.h>
 #include "uart.h"
+#include "gpio.h"
 
 int main(){
+
+   gpio_init();
+   uart_init();
+   char A = "A";
+   char B = "B";
+   int light_on = 0;
+
     while (1)
-    {
+    {   
+       
+        int buttn_A(){
+            return((GPIO0->IN & (1<<14)));
+        }
+        int buttn_B(){
+            return((GPIO0->IN & (1<< 23)));
+        }
+
+        if (buttn_A() == 0){
+            uart_send(A);
+        }
+        if (buttn_B() == 0){
+            uart_send(B);
+        }
+
+
+        if ((light_on == 0) & (uart_read() != "\0"))
+        {
+            gpio_lights_on();
+            light_on = 1;
+
+        } else if( (light_on == 1) & (uart_read() != "\0"))
+        {
+            gpio_lights_off();
+            light_on = 0;
+        }
         
+
     }
     return 0;
     
--- 2_uart/uart.h
+++ 2_uart/uart.h
@@ -46,3 +46,4 @@ typedef struct
 void uart_init();
 void uart_send(char letter);
 char uart_read();
+
